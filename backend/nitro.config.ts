13bb834193bba1ab9b865d4c39a3418d8b1a37a4
David parys <parys.david@gmail.com>
2025-09-03 19:16:11 +0200

    chore: update build script and modify nitro.config preset

--- backend/nitro.config.ts
+++ backend/nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from 'nitropack/config'
 export default defineNitroConfig({
     compatibilityDate: "2025-07-15",
     srcDir: "src",
-    preset: "vercel",
+    // preset: "vercel",
     vercel: {
         config: {
             bypassToken: process.env.VERCEL_BYPASS_TOKEN
